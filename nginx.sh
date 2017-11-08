c0ae7794e21aae8baee0d2eb3c686ac0e02ad01a
rffanlab <admin@rffan.com>
2017-11-08 09:18:46 +0800

    更新了Nginx脚本。 添加了支持64位的Linux的安装phantomjs的系统
    Signed-off-by: rffanlab <admin@rffan.com>

---
+++ nginx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+yum install deltarpm -y
+yum install -y redhat-lsb
+yum install -y wget
+osrelease=$(lsb_release -rs|awk -F'.' '{print $1}')
+cat >/etc/yum.repos.d/nginx.repo<<EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/$osrelease/\$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y nginx
---
+++ phantomjs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# this script is written for setting up phantomjs env for webdriver to run.
+# written by Rffanlab
+# Any question please visit https://ddns.togit.cc:7777/rffanlab/tools to raise an issue
+wget -c https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+# wget -c https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local
+mv /usr/local/phantomjs-2.1.1-linux-x86_64 /usr/local/phantomjs
+echo "export PHANTOMJS=/usr/local/phantomjs">>/etc/profile
+echo "export PATH=\$PATH:\$PHANTOMJS/bin">>/etc/profile
+
