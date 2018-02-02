43cc3567bb2f20696c55c45c3fdc8066b6082e58
rffanlab <admin@rffan.com>
2018-02-02 14:00:47 +0800

    增加了安装python 3.6的脚本 修复了安装python3.5的时候软链接是2.7的bug

--- shells/installpy36.sh
+++ shells/installpy36.sh
@@ -3,11 +3,11 @@
 #yum 安装制定安装包，如果以后更新后使用dnf，可能需要更新该脚本，不过CentOS7 目前还是使用的yum
 yum install -y wget openssl* gcc*
 # 下载安装包，如果有更新，请去python官网更新下载网址
-wget -c https://www.python.org/ftp/python/3.5.4/Python-3.5.4.tgz
-tar zxvf Python-3.5.4.tgz
-cd Python-3.5.4
+wget -c https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+tar zxvf Python-3.6.4.tgz
+cd Python-3.6.4
 # 之所以不开启优化，是怕出问题，如果你不怕，可以自己添加
-./configure --prefix=/usr/local/python35
+./configure --prefix=/usr/local/python36
 make && make install
-ln -s /usr/local/python35/bin/python /usr/bin/python35
-ln -s /usr/local/python35/bin/pip3 /usr/bin/pip3
+ln -s /usr/local/python36/bin/python /usr/bin/python3
+ln -s /usr/local/python36/bin/pip3 /usr/bin/pip3
