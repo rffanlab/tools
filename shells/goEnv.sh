92efd4d109fe24a2d6e29a9a5ef6759a0f01b2b8
rffanlab <admin@rffan.com>
2018-02-13 14:18:17 +0800

    go环境新增了设置完成时的脚本提示语

--- shells/goEnv.sh
+++ shells/goEnv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # this script only fit centos
+#
 yum install -y wget
 wget -c https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz
 tar zxvf go1.9.4.linux-amd64.tar.gz
@@ -11,6 +12,7 @@ export GOROOT=/usr/local/go
 export GOPATH=/home/go/gopath:/home/go/ugopath
 export PATH=\$PATH:\$GOROOT/bin:/home/go/gopath/bin:/home/go/ugopath/bin
 EOF
-
+echo "请在结束完成之后使用 source /etc/profile 来让环境变量生效"
+echo "Please use command: source /etc/profile    to make path var take effect"
 
 
