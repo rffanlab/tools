05f258f3ffdb1dfbc31914bd228aaba659058f48
rffanlab <admin@rffan.com>
2018-02-25 10:43:42 +0800

    添加了let's Encrypt的脚本

---
+++ shells/ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install -y python-devel python-setuptools && easy_install pip
+pip install certbot
+echo "请输入域名"
+read domain
+echo "请输入附加域名"
+read additionalDomain
+echo "请输入邮箱"
+read email
+echo "请输入网站存储路径"
+read path
+certbot certonly --email ${email} --agree-tos --webroot -w ${path} -d ${domain} -d ${additionalDomain}
