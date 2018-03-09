bf13194ed3e6974435745b500f75a0d9eb342cf7
rffanlab <admin@rffan.com>
2018-03-09 09:35:58 +0800

    Signed-off-by: rffanlab <admin@rffan.com>

--- shells/ssl.sh
+++ shells/ssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum install -y python-devel python-setuptools && easy_install pip
+yum install cronie
 pip install certbot
 echo "请输入域名"
 read domain
@@ -10,3 +11,12 @@ read email
 echo "请输入网站存储路径"
 read path
 /usr/bin/certbot certonly --email ${email} --agree-tos --webroot -w ${path} -d ${domain} -d ${additionalDomain}
+
+count=$(crontab -l | grep '/usr/bin/certbot'| wc -l) 
+if [[ "${count}" = "0" ]] ;then 
+	# 如果你的使用ssl的并不是nginx web服务器，请将下面这一句引号里的/usr/sbin/nginx -s reload 替换成你需要的重载服务器的命令
+	echo "0 3 */7 * * /usr/bin/certbot renew --disable-hook-validation --renew-hook \"/usr/sbin/nginx -s reload\"">> /var/spool/cron/root
+	echo "还没有添加cron" 
+fi
+
+
