ba32ff358d288bccba5bdb5fa2e63c0de6e09358
rffanlab <admin@rffan.com>
2018-04-10 10:43:08 +0800

    修改了添加了开放防火墙端口，开放tcp和udp 修改了文件，将配置，例如端口，密码以及加密方法 放置在了开头，方便小伙伴们使用

--- shells/systemdsss.sh
+++ shells/systemdsss.sh
@@ -1,8 +1,12 @@
-#！/bin/bash
-# This script is written for Setup Shadowsock server for users.
+#!/usr/bin/env bash
+# This script is written for Setup Shadowsock server for users for CentOS7.
 yum install -y git wget python-devel python-setuptools
 easy_install pip
 pip install shadowsocks
+ssPort=10040
+ssPass=rffanlab
+ssMethod=rc4-md5
+
 cat >/usr/lib/systemd/system/ss-server.service <<EOF
 [Unit]
 Description=ShadowSocks Server
@@ -10,7 +14,7 @@ Description=ShadowSocks Server
 [Service]
 Type=forking
 LimitNOFILE=65536
-ExecStart=/usr/bin/ssserver -p 10040 -k rffanlab -m rc4-md5 --user nobody -d start
+ExecStart=/usr/bin/ssserver -p $ssPort -k $ssPass -m $ssMethod --user nobody -d start
 ExecStop=/usr/bin/ssserver -d stop
 Restart=on-abort
 
@@ -21,3 +25,7 @@ systemctl daemon-reload
 systemctl enable ss-server
 systemctl start ss-server
 systemctl status ss-server
+
+firewall-cmd --zone=public --permanent --add-port=$ssPort/tcp
+firewall-cmd --zone=public --permanent --add-port=$ssPort/udp
+firewall-cmd --reload
